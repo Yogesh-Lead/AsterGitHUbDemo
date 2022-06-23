c1e58372d3aaec5e202fc96d6d51f117b3b0a3d6
Yogesh-Lead <yogesh.moremcoe@gmail.com>
2022-06-23 14:55:59 +0530

    First Commit

---
+++ src/test/java/com/astergitdemo/DemoAster1.java
@@ -0,0 +1,17 @@
+package com.astergitdemo;
+
+public class DemoAster1 {
+	
+	public static void main(String[] args) {
+
+		String hungrey = "Yes";
+		if (hungrey == "Yes") {
+			System.out.println("Eat something");
+		} else {
+			System.out.println("Do some work");
+		}
+
+	}
+
+
+}
